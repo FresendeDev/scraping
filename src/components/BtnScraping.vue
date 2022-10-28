2742e73086c9672fc775ae578b697e9bd2674be9
Fabio Resende <fabio.resende@gmail.com>
2022-10-28 20:25:22 -0300

    iniciando testes

---
+++ src/components/BtnScraping.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="btn-scr">
+    <button>Scraping</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BtnScraping",
+};
+</script>
+
+<style lang="sass" scoped>
+.btn-scr
+  padding: 20px
+</style>
