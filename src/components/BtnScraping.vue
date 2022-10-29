ba7282e3f009ec23afc7c151cee765e7a013a129
Fabio Resende <fabio.resende@gmail.com>
2022-10-29 18:57:57 -0300

    resolvendo um bug

--- src/components/BtnScraping.vue
+++ src/components/BtnScraping.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="btn-scr">
+    <!-- <button @click="ScrapJ">Scraping</button> -->
     <button>Scraping</button>
   </div>
 </template>
 
 <script>
+// import ScrapJ from "../scraping/scraping-amazon";
+
 export default {
   name: "BtnScraping",
+  methods: {
+    //  ScrapJ
+  },
+  data() {},
 };
 </script>
 
